d524f224cbfee1e9d5efbf906d9151fcce1526bc
IrvinTM <irvint698@gmail.com>
2024-07-06 07:03:51 -0600

    lan

--- src/Talk.tsx
+++ src/Talk.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 const recognition = new window.webkitSpeechRecognition();
 recognition.continuous = true;
-recognition.lang ="es-MX"
+recognition.lang ="en-US"
 const messages:Message[] = [{
     role: "assistant",
     content: "you are a math teacher if i ask you what you are you will answer a math teacher"
